2ff74ffaa42749c5675bb38f427bf14170418225
Shashank Dubey <shashank02.dubey@gmail.com>
2023-06-02 23:00:13 +0530

    Second Commit

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -28,21 +28,26 @@ class _MainScreenState extends State<MainScreen> {
                   ),
                 ),
                 const SizedBox(width: 160),
-                Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: Colors.deepPurple,
-                      width: 2,
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.deepPurple,
+                        width: 2,
+                      ),
                     ),
-                  ),
-                  child: const CircleAvatar(
-                    radius: 30,
-                    backgroundColor: Colors.white,
-                    child: Icon(
-                      Icons.person,
-                      color: Colors.deepPurple,
-                      size: 40,
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.white,
+                      child: Image.asset(
+                        'images/exit.png',
+                        width: 25,
+                        height: 25,
+                      ),
                     ),
                   ),
                 ),
@@ -54,19 +59,88 @@ class _MainScreenState extends State<MainScreen> {
                 Expanded(
                   child: ReusableCard(
                     rang: Colors.deepPurple,
-                    cardChild: Container(
-                      height: 200,
-                      decoration: BoxDecoration(
-                        color: Colors.deepPurple,
-                        borderRadius: BorderRadius.circular(20),
+                    cardChild: TextButton(
+                      onPressed: () {},
+                      child: Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Image.asset(
+                              'images/medicine.png',
+                              height: 75,
+                              width: 75,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(height: 20),
+                            const Text(
+                              'Medical History',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              'Medical procedures',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
                 const SizedBox(width: 5),
-                const Expanded(
+                Expanded(
                   child: ReusableCard(
                     rang: Colors.deepPurple,
+                    cardChild: TextButton(
+                      onPressed: () {},
+                      child: Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Image.asset(
+                              'images/avatar.png',
+                              height: 75,
+                              width: 75,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(height: 15),
+                            const Text(
+                              'Profile',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              'View your profile',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
@@ -75,16 +149,92 @@ class _MainScreenState extends State<MainScreen> {
               height: 5,
             ),
             Row(
-              children: const <Widget>[
+              children: <Widget>[
                 Expanded(
                   child: ReusableCard(
                     rang: Colors.deepPurple,
+                    cardChild: TextButton(
+                      onPressed: () {},
+                      child: Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Image.asset(
+                              'images/insurance.png',
+                              height: 75,
+                              width: 75,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(height: 15),
+                            const Text(
+                              'CIBIL',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              'View Your current CIBIL Score',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-                SizedBox(width: 5),
+                const SizedBox(width: 5),
                 Expanded(
                   child: ReusableCard(
                     rang: Colors.deepPurple,
+                    cardChild: TextButton(
+                      onPressed: () {},
+                      child: Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Image.asset(
+                              'images/payment.png',
+                              height: 75,
+                              width: 75,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(height: 15),
+                            const Text(
+                              'Bills',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              'View your past medical bills',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
@@ -93,16 +243,92 @@ class _MainScreenState extends State<MainScreen> {
               height: 5,
             ),
             Row(
-              children: const <Widget>[
+              children: <Widget>[
                 Expanded(
                   child: ReusableCard(
                     rang: Colors.deepPurple,
+                    cardChild: TextButton(
+                      onPressed: () {},
+                      child: Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Image.asset(
+                              'images/discussion.png',
+                              height: 75,
+                              width: 75,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(height: 15),
+                            const Text(
+                              'Doctors',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              'Book your E-appointment',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-                SizedBox(width: 5),
+                const SizedBox(width: 5),
                 Expanded(
                   child: ReusableCard(
                     rang: Colors.deepPurple,
+                    cardChild: TextButton(
+                      onPressed: () {},
+                      child: Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Image.asset(
+                              'images/vaccine.png',
+                              height: 75,
+                              width: 75,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(height: 15),
+                            const Text(
+                              'Vaccines',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              'View Current Vaccine details',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
